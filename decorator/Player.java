61d8c5a097c63a02643fecc801a7c3ff2522ccf3
Soraya Remaili <sorayaremaili@gmail.com>
2024-01-28 18:16:30 -0500

    adding provided decorator

--- decorator/Player.java
+++ decorator/Player.java
@@ -1,21 +1,39 @@
 package decorator;
 import java.util.*;
+
+/**
+ * A Player.
+ * @author Soraya Remaili
+ */
 public abstract class Player 
 {
 	protected String name;
 	protected ArrayList<String> lines;
 
+	/**
+	 * Creates a new Player with the given lines and name.
+	 * @param lines The ArrayList version of the .txt file representation of the Player.
+	 * @param name The name of the Player.
+	 */
 	public Player(ArrayList<String> lines, String name)
 	{
 		this.name = name;
 		this.lines = lines;
 	}
 
+	/**
+	 * Creates a string representation of the Player's name.
+	 * @return A string representation of the name.
+	 */
 	public String getName()
 	{
 		return name;
 	}
 
+	/**
+	 * Creates a formatted string representation of the Player's lines.
+	 * @return A string representation of the Player.
+	 */
 	public String toString()
 	{
 		String s = "";
--- decorator/PlayerDecorator.java
+++ decorator/PlayerDecorator.java
@@ -1,17 +1,28 @@
 package decorator;
-
 import java.util.ArrayList;
 
+/**
+ * The decorator class for a Player.
+ * @author Soraya Remaili
+ */
 public abstract class PlayerDecorator extends Player
 {
 	private Player player;
 
+	/**
+	 * Creates a new PlayerDecorator with the given player.
+	 * @param player The player to be decorated.
+	 */
 	public PlayerDecorator(Player player)
 	{
 		super(player.lines, player.name);
 		this.player = player;
 	}
 
+	/**
+	 * Adds the given decor to the Player.
+	 * @param decor The decor to be added to the Player.
+	 */
 	protected void integrateDecor(ArrayList<String> decor)
 	{
 		for(int i = 0; i<lines.size(); i++)
