1af6c2bcaa4219158960bbdc7b4c557f8386d123
Soraya Remaili <sorayaremaili@gmail.com>
2024-02-08 21:53:34 -0500

    adding

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
+/**
+ * The FlashCards containing Words.
+ * @author Soraya Remaili
+ */
 public class FlashCards 
 {
     private Random rand;
@@ -7,12 +11,19 @@ public class FlashCards
     private ArrayList<Word> words;
     private Word currentWord;
 
+    /**
+     * Creates a new FlashCards object and initializes some of its fields.
+     */
     private FlashCards()
     {
         rand = new Random();
         words = FileReader.getWords();
     }
 
+    /**
+     * Creates a FlashCards object if one does not exist and returns the FlashCards object.
+     * @return The FlashCards object flashcards.
+     */
     public static FlashCards getInstance()
     {
         if(flashCards == null)
@@ -22,6 +33,10 @@ public class FlashCards
         return flashCards;
     }
 
+    /**
+     * Creates a string representation of a randomly selected a word from the FlashCards's words and updates the currentWord.
+     * @return A string representation of the random word.
+     */
     public Word getWord()
     {
         currentWord = (words.get(rand.nextInt(words.size())));
--- singleton/Word.java
+++ singleton/Word.java
@@ -1,3 +1,7 @@
+/**
+ * The Word of a FlashCard.
+ * @author Soraya Remaili
+ */
 public class Word 
 {
     private String word;
@@ -5,6 +9,13 @@ public class Word
     private String definition;
     private String sentence;
     
+    /**
+     * Creates a new Word with the specified properties.
+     * @param word The name of the Word to be created.
+     * @param type The part of speech of the Word.
+     * @param definition The definition of the Word.
+     * @param sentence A sentence containing the Word.
+     */
     public Word(String word, String type, String definition, String sentence)
     {
         this.word = word;
@@ -13,11 +24,19 @@ public class Word
         this.sentence = sentence;
     }
 
+    /**
+     * Creates a string representation of the Word itself.
+     * @return A string representation of the Word.
+     */
     public String getWord()
     {
         return word;
     }
 
+    /**
+     * Creates a formatted String representation of the type, definition, and the sentence fields.
+     * @return A String representation of the Word's properties.
+     */
     public String getDescription()
     {
         return "Part of Speech: " + type + "\n" + "Definition: " + definition + "\n" + sentence;
